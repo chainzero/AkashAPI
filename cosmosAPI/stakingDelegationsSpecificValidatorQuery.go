efcf4288e227bc10b7d15e2ed06011139e56b564
ChainZero <chainzero239@gmail.com>
2021-10-18 09:49:08 -0400

    adding delegations query via specific validator API

---
+++ cosmosAPI/stakingDelegationsSpecificValidatorQuery.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type DelegationDetail struct {
+	Validator struct {
+		OperatorAddress string `json:"operator_address"`
+		ConsensusPubkey struct {
+			Type string `json:"@type"`
+			Key  string `json:"key"`
+		} `json:"consensus_pubkey"`
+		Jailed          bool   `json:"jailed"`
+		Status          string `json:"status"`
+		Tokens          string `json:"tokens"`
+		DelegatorShares string `json:"delegator_shares"`
+		Description     struct {
+			Moniker         string `json:"moniker"`
+			Identity        string `json:"identity"`
+			Website         string `json:"website"`
+			SecurityContact string `json:"security_contact"`
+			Details         string `json:"details"`
+		} `json:"description"`
+		UnbondingHeight string    `json:"unbonding_height"`
+		UnbondingTime   time.Time `json:"unbonding_time"`
+		Commission      struct {
+			CommissionRates struct {
+				Rate          string `json:"rate"`
+				MaxRate       string `json:"max_rate"`
+				MaxChangeRate string `json:"max_change_rate"`
+			} `json:"commission_rates"`
+			UpdateTime time.Time `json:"update_time"`
+		} `json:"commission"`
+		MinSelfDelegation string `json:"min_self_delegation"`
+	} `json:"validator"`
+}
+
+func main() {
+	// Replace delegator and validator ID values in function call template below
+	stakingDelegationValidatorQuery("akash1w3k6qpr4uz44py4z68chfrl7ltpxwtkngnc6xk", "akashvaloper18tmu0lrfsdvke8e3a3jsd7fq2rs29krfkkw8gz")
+}
+
+func stakingDelegationValidatorQuery(delegatorID, validatorID string) {
+	method := "GET"
+
+	url := "http://135.181.60.250:1317/cosmos/staking/v1beta1/delegators/" + delegatorID + "/validators/" + validatorID
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	DelegationDetail1 := DelegationDetail{}
+
+	json.Unmarshal(body, &DelegationDetail1)
+
+	fmt.Println(DelegationDetail1)
+
+}
