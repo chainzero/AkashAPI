ab361411e2e0ae573b2d8fda8dc9300d93100461
ChainZero <chainzero239@gmail.com>
2021-10-27 10:25:38 -0400

    adding validator targeted query api

---
+++ cosmosAPI/validatorsTargetedQuery.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type ValidatorDetail struct {
+	Validator struct {
+		OperatorAddress string `json:"operator_address"`
+		ConsensusPubkey struct {
+			Type string `json:"@type"`
+			Key  string `json:"key"`
+		} `json:"consensus_pubkey"`
+		Jailed          bool   `json:"jailed"`
+		Status          string `json:"status"`
+		Tokens          string `json:"tokens"`
+		DelegatorShares string `json:"delegator_shares"`
+		Description     struct {
+			Moniker         string `json:"moniker"`
+			Identity        string `json:"identity"`
+			Website         string `json:"website"`
+			SecurityContact string `json:"security_contact"`
+			Details         string `json:"details"`
+		} `json:"description"`
+		UnbondingHeight string    `json:"unbonding_height"`
+		UnbondingTime   time.Time `json:"unbonding_time"`
+		Commission      struct {
+			CommissionRates struct {
+				Rate          string `json:"rate"`
+				MaxRate       string `json:"max_rate"`
+				MaxChangeRate string `json:"max_change_rate"`
+			} `json:"commission_rates"`
+			UpdateTime time.Time `json:"update_time"`
+		} `json:"commission"`
+		MinSelfDelegation string `json:"min_self_delegation"`
+	} `json:"validator"`
+}
+
+func main() {
+	// Replace Validator Account value in function call template below
+	validatorsTargetedQuery("akashvaloper1qvsus5qg8yhre7k2c78xkkw4nvqqgev7zw5wzs")
+}
+
+func validatorsTargetedQuery(id string) {
+	method := "GET"
+
+	url := "http://135.181.60.250:1317/cosmos/staking/v1beta1/validators/" + id
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	ValidatorDetail1 := ValidatorDetail{}
+
+	json.Unmarshal(body, &ValidatorDetail1)
+
+	fmt.Println(ValidatorDetail1)
+
+}
